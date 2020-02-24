d4f475f9734dada60eee8f470f5c1286789c45d1
Lomba8 <jack.james.lomba@gmail.com>
2020-02-24 23:47:15 +0100

    area_studenti cards
    Former-commit-id: d60422e7facdb8b39822cb30f700068b6b68a306

--- lib/preferences/globals.dart
+++ lib/preferences/globals.dart
@@ -10,9 +10,11 @@ class Globals with ChangeNotifier {
     _themeMode = mode;
     final prefs = await SharedPreferences.getInstance();
 
-    (mode.toString() == 'ThemeMode.dark')
+    (mode == ThemeMode.dark)
         ? prefs.setBool('DarkMode', true)
         : prefs.setBool('DarkMode', false);
+
+    if (mode == ThemeMode.system) prefs.setBool('DarkMode', null);
 
     print(mode.toString());
 
@@ -106,5 +108,83 @@ class Globals with ChangeNotifier {
 
   static Color violaScolorito = Color.fromRGBO(115, 121, 247, 0.7);
 
+  static Map<String, Icon> icone = {
+    'Autogestione': Icon(Icons.map), // TODO: scegliere icone
+    'Alternanza':
+        Icon(Icons.headset_mic), // headset_mic, work, home_work, euro, weekend
+    'Bacheca': Icon(Icons
+        .markunread_mailbox), // drafts, description, markunread_mailbox, inbox, attach_file, local_post_office, insert_drive_file
+    'Tutoraggi': Icon(Icons
+        .person_add), // people, accessibility, how_to_reg, emoji_people, group_add, group, people_outline
+    'Note': Icon(Icons.sentiment_dissatisfied), //feedback,
+    'App Panini': Icon(Icons.smartphone),
+  };
+
   static Color rosso = Color.fromRGBO(79, 20, 17, 1);
+
+  static Map<String, Map<String, dynamic>> sezioni = {
+    'arancione': {
+      'color': Colors.deepOrangeAccent, //Color.fromRGBO(241, 152, 93, 0.7),
+      'textColor': Colors.deepOrangeAccent
+          .withOpacity(0.7), //Color.fromRGBO(241, 152, 93, 0.5),
+      'gradientColors': [
+        Colors.orange[800]
+            .withOpacity(0.8), //Color.fromRGBO(238, 142, 84, 1.0),
+        Colors.orange[600]
+            .withOpacity(0.8), //Color.fromRGBO(242, 158, 104, 1.0),
+        Colors.orange[400]
+            .withOpacity(0.8), //Color.fromRGBO(246, 193, 156, 1.0),
+      ],
+    },
+    'blu': {
+      'color': Color.fromRGBO(92, 130, 247, 0.7),
+      'textColor': Color.fromRGBO(92, 130, 247, 0.7),
+      'gradientColors': [
+        Color.fromRGBO(80, 122, 246, 1.0),
+        Color.fromRGBO(110, 146, 247, 1.0),
+        Color.fromRGBO(141, 173, 248, 1.0),
+      ],
+    },
+    'rosa': {
+      'color': Colors.purpleAccent, //Color.fromRGBO(186, 95, 192, 0.7),
+      'textColor': Colors.purpleAccent
+          .withOpacity(0.7), //Color.fromRGBO(186, 95, 192, 0.5),
+      'gradientColors': [
+        Colors.purpleAccent[400]
+            .withOpacity(0.8), // Color.fromRGBO(233, 90, 219, 1.0),
+        Colors.purpleAccent
+            .withOpacity(0.8), // Color.fromRGBO(236, 124, 228, 1.0),
+        Colors.purpleAccent[100]
+            .withOpacity(0.8), // Color.fromRGBO(239, 163, 233, 1.0),
+      ],
+    },
+    'rosso': {
+      'color': Colors.redAccent[400].withOpacity(0.9),
+      'textColor': Colors.redAccent[400].withOpacity(0.6),
+      'gradientColors': [
+        Colors.redAccent[400]
+            .withOpacity(0.9), // Color.fromRGBO(236, 77, 69, 1.0),
+        Colors.redAccent[400], // Color.fromRGBO(237, 103, 98, 1.0),
+        Colors.redAccent, // Color.fromRGBO(239, 135, 132, 1.0),
+      ],
+    },
+    'verde': {
+      'color': Color.fromRGBO(144, 237, 137, 0.7),
+      'textColor': Color.fromRGBO(144, 237, 137, 0.5),
+      'gradientColors': [
+        Color.fromRGBO(21, 195, 65, 1.0),
+        Color.fromRGBO(46, 208, 81, 1.0),
+        Color.fromRGBO(115, 247, 124, 1.0),
+      ],
+    },
+    'viola': {
+      'color': Color.fromRGBO(126, 121, 206, 0.7),
+      'textColor': Color.fromRGBO(126, 121, 206, 0.9),
+      'gradientColors': [
+        Color.fromRGBO(115, 91, 245, 1.0),
+        Color.fromRGBO(139, 115, 244, 1.0),
+        Color.fromRGBO(162, 137, 247, 1.0),
+      ],
+    },
+  };
 }
