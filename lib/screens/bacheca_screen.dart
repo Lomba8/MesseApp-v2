c12ad54669bab1dbf28b58240e801b73aa8b38b3
Lomba8 <jack.james.lomba@gmail.com>
2020-03-20 18:46:06 +0100

    changed pdf view & changed page indicator

--- lib/screens/bacheca_screen.dart
+++ lib/screens/bacheca_screen.dart
@@ -10,11 +10,14 @@ import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flappy_search_bar/search_bar_style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+import 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
 import 'package:flutter_share/flutter_share.dart';
+import 'package:flutter_full_pdf_viewer/full_pdf_viewer_plugin.dart';
+
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:native_pdf_view/native_pdf_view.dart';
 
 class BachecaScreen extends StatefulWidget {
   @override
@@ -59,6 +62,7 @@ class _BachecaScreenState extends State<BachecaScreen> {
                           : Colors.black54,
                     ),
                     onPressed: () {
+                      if (_loading) _loading = !_loading;
                       Navigator.pop(context);
                     }),
                 title: Text(
@@ -194,17 +198,20 @@ class _BachecaScreenState extends State<BachecaScreen> {
                                                   await c.downloadPdf();
                                               _loading = false;
                                               _pathh = _pathh.path;
-                                              setState(() {});
-                                              Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      PDFScreen(
-                                                    path: _pathh,
-                                                    title: encodePath(c.title),
+                                              if (mounted) {
+                                                setState(() {});
+                                                Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        PDFScreen(
+                                                      path: _pathh,
+                                                      title:
+                                                          encodePath(c.title),
+                                                    ),
                                                   ),
-                                                ),
-                                              );
+                                                );
+                                              }
                                             },
                                     ),
                                     trailing: (isExpanded) => AnimatedCrossFade(
@@ -221,7 +228,7 @@ class _BachecaScreenState extends State<BachecaScreen> {
                                       padding: const EdgeInsets.all(10.0),
                                       child: c.content == null
                                           ? Container(
-                                              height: 1,
+                                              height: 2,
                                               child: LinearProgressIndicator(
                                                 value: null,
                                                 backgroundColor:
@@ -275,32 +282,129 @@ class _BachecaScreenState extends State<BachecaScreen> {
       );
 }
 
-class PDFScreen extends StatefulWidget {
-  final String path, title;
+// class PDFScreen extends StatefulWidget {
+//   final String path, title;
 
-  PDFScreen({Key key, this.path, this.title}) : super(key: key);
+//   PDFScreen({Key key, this.path, this.title}) : super(key: key);
 
-  _PDFScreenState createState() => _PDFScreenState();
-}
+//   _PDFScreenState createState() => _PDFScreenState();
+// }
 
-class _PDFScreenState extends State<PDFScreen> {
-  Future<PDFDocument> _getDocument(path) async {
-    return PDFDocument.openFile(path);
-  }
+// class _PDFScreenState extends State<PDFScreen> {
+//   Future<PDFDocument> _getDocument(path) async {
+//     return PDFDocument.openFile(path);
+//   }
 
-  int _page = 1;
-  PageController _controller;
+//   int _page = 1;
+//   PageController _controller;
+
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         appBar: PreferredSize(
+//           preferredSize: Size.fromHeight(120.0),
+//           child: AppBar(
+//             backgroundColor: Theme.of(context).accentColor,
+//             flexibleSpace: Container(
+//               margin: EdgeInsets.fromLTRB(60.0, 60.0, 60.0, 0.0),
+//               child: Text(
+//                 widget.title,
+//                 maxLines: 5,
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   color: Colors.black,
+//                   fontSize: 13.0,
+//                   wordSpacing: 1.2,
+//                 ),
+//                 overflow: TextOverflow.ellipsis,
+//                 softWrap: true,
+//               ),
+//             ),
+//             actions: <Widget>[
+//               IconButton(
+//                 icon: Padding(
+//                   padding: EdgeInsets.only(right: 20.0),
+//                   child: Icon(
+//                       Platform.isAndroid ? Icons.share : CupertinoIcons.share),
+//                 ),
+//                 onPressed: () {
+//                   FlutterShare.shareFile(
+//                       title: widget.title, filePath: widget.path);
+//                 },
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: FutureBuilder<PDFDocument>(
+//           future: _getDocument(widget.path),
+//           builder: (_, snapshot) {
+//             if (snapshot.hasData) {
+//               return Stack(
+//                 children: [
+//                   PDFView(
+//                     document: snapshot.data,
+//                     onPageChanged: (int page) {
+//                       setState(() {
+//                         _page = page;
+//                       });
+//                     },
+//                     scrollDirection: Axis.horizontal,
+//                     controller: _controller,
+//                     renderer: (PDFPage page) => page.render(
+//                       // cropRect: rect,
+//                       width: page.width * 2,
+//                       height: page.height * 2,
+//                       format: PDFPageFormat.JPEG,
+//                       backgroundColor:
+//                           MediaQuery.platformBrightnessOf(context) ==
+//                                   Brightness.dark
+//                               ? '##1c1c27'
+//                               : '#efeef5',
+//                     ),
+//                   ),
+//                   Positioned(
+//                     left: 15,
+//                     top: 15,
+//                     child: Container(
+//                         child: Text(
+//                       _page.toString(),
+//                       style: TextStyle(color: Colors.black54, fontSize: 14),
+//                     )),
+//                   ),
+//                 ],
+//               );
+//             }
+
+//             if (snapshot.hasError) {
+//               return Center(
+//                 child: Text(
+//                   'PDF Rendering does not '
+//                   'support on the system of this version',
+//                 ),
+//               );
+//             }
+
+//             return Center(child: CircularProgressIndicator());
+//           },
+//         ),
+//       );
+// }
+
+class PDFScreen extends StatelessWidget {
+  final String path;
+  final String title;
+  PDFScreen({this.path, this.title});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) {
+    return PDFViewerScaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(120.0),
           child: AppBar(
             backgroundColor: Theme.of(context).accentColor,
             flexibleSpace: Container(
-              margin: EdgeInsets.fromLTRB(60.0, 60.0, 60.0, 0.0),
+              margin: EdgeInsets.fromLTRB(60.0, 40.0, 60.0, 0.0),
               child: Text(
-                widget.title,
+                title,
                 maxLines: 5,
                 textAlign: TextAlign.center,
                 style: TextStyle(
@@ -316,68 +420,20 @@ class _PDFScreenState extends State<PDFScreen> {
               IconButton(
                 icon: Padding(
                   padding: EdgeInsets.only(right: 20.0),
-                  child: Icon(
-                      Platform.isAndroid ? Icons.share : CupertinoIcons.share),
+                  child: Platform.isAndroid
+                      ? Icon(Icons.share)
+                      : Icon(
+                          CupertinoIcons.share,
+                          size: 25.0,
+                        ),
                 ),
                 onPressed: () {
-                  FlutterShare.shareFile(
-                      title: widget.title, filePath: widget.path);
+                  FlutterShare.shareFile(title: title, filePath: path);
                 },
               ),
             ],
           ),
         ),
-        body: FutureBuilder<PDFDocument>(
-          future: _getDocument(widget.path),
-          builder: (_, snapshot) {
-            if (snapshot.hasData) {
-              return Stack(
-                children: [
-                  PDFView(
-                    document: snapshot.data,
-                    onPageChanged: (int page) {
-                      setState(() {
-                        _page = page;
-                      });
-                    },
-                    scrollDirection: Axis.horizontal,
-                    controller: _controller,
-                    renderer: (PDFPage page) => page.render(
-                      // cropRect: rect,
-                      width: page.width * 2,
-                      height: page.height * 2,
-                      format: PDFPageFormat.JPEG,
-                      backgroundColor:
-                          MediaQuery.platformBrightnessOf(context) ==
-                                  Brightness.dark
-                              ? '##1c1c27'
-                              : '#efeef5',
-                    ),
-                  ),
-                  Positioned(
-                    left: 15,
-                    top: 15,
-                    child: Container(
-                        child: Text(
-                      _page.toString(),
-                      style: TextStyle(color: Colors.black54, fontSize: 15),
-                    )),
-                  ),
-                ],
-              );
-            }
-
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  'PDF Rendering does not '
-                  'support on the system of this version',
-                ),
-              );
-            }
-
-            return Center(child: CircularProgressIndicator());
-          },
-        ),
-      );
+        path: path);
+  }
 }
--- lib/screens/orari_screen.dart
+++ lib/screens/orari_screen.dart
@@ -1,12 +1,8 @@
-import 'package:Messedaglia/screens/menu_screen.dart';
 import 'package:Messedaglia/screens/orari_section/lessons_section.dart';
 import 'package:Messedaglia/screens/orari_section/orari_section.dart';
-import 'package:Messedaglia/utils/orariUtils.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'orari_section/orari_section.dart';
 
 class Orari extends StatefulWidget {
   static final String id = 'orari_screen';
@@ -18,205 +14,52 @@ class Orari extends StatefulWidget {
 }
 
 class _OrariState extends State<Orari> {
-  int _index = 1;
-  PageController _controller = PageController();
-  int _page = 0;
-  double _pageDouble = 0.0;
   @override
   void initState() {
-    _controller.addListener(_listener);
     super.initState();
   }
 
-  _listener() {
-    _pageDouble = _controller?.page;
-    if (_controller.position.userScrollDirection == ScrollDirection.reverse) {
-      if (_controller.position.atEdge) {
-        _page = 1;
-        setState(() {});
-      }
-      // print('swiped to right');
-    } else {
-      if (_controller.position.atEdge) {
-        _page = 0;
-        setState(() {});
-      }
-
-      //print('swiped to left');
-    }
-  }
+  int _index = 1;
+  PageController _controller = PageController();
 
   @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          brightness: Theme.of(context).brightness,
-          title: AnimatedCrossFade(
-            firstChild: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                if (downloading)
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 4),
-                    child: SizedBox(
-                      height: 18.0,
-                      width: 18.0,
-                      child: CircularProgressIndicator(
-                        value: null,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                      ),
-                    ),
-                  ),
-                if (downloading)
-                  SizedBox(
-                    width: 2.0,
-                  ),
-                Text(
-                  downloading ? 'RARI' : 'ORARI',
-                  style: TextStyle(
-                      color: Theme.of(context).brightness == Brightness.light
-                          ? Colors.black
-                          : Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            secondChild: Row(
-              children: [
-                SizedBox(
-                  width: 79,
-                ),
-                Text(
-                  'LEZIONI',
-                  style: TextStyle(
-                      color: Theme.of(context).brightness == Brightness.light
-                          ? Colors.black
-                          : Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            crossFadeState: _pageDouble < 0.5
-                ? CrossFadeState.showFirst
-                : CrossFadeState.showSecond,
-            duration: Duration(milliseconds: 400),
-            layoutBuilder:
-                (topChild, topChildKey, bottomChild, bottomChildKey) {
-              return Stack(
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.center,
-                    child: topChild,
-                    key: topChildKey,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: bottomChild,
-                    key: bottomChildKey,
-                  ),
-                ],
-              );
-            },
-          ),
-          leading: _page == 0
-              ? Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: IconButton(
-                    icon: Icon(Icons.settings_backup_restore),
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.white54
-                        : Colors.black54,
-                    onPressed: () {
-                      resetprefs();
-                      setState(() {});
-                    },
-                  ),
-                )
-              : Offstage(),
-          actions: <Widget>[
-            _page == 0
-                ? Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: IconButton(
-                        icon: Icon(
-                          Icons.file_download,
-                          color:
-                              Theme.of(context).brightness == Brightness.light
-                                  ? Colors.black54
-                                  : Colors.white54,
-                        ),
-                        onPressed: selectedClass == null
-                            ? null
-                            : () async {
-                                downloadOrario(selectedClass).then((_) {
-                                  showNotificationWithDefaultSound(
-                                      selectedClass);
-                                });
-
-                                setState(() {
-                                  downloading = true;
-                                  progress = 0;
-                                });
-                              }),
-                  )
-                : Offstage(),
-          ],
-          centerTitle: true,
-          pinned: true,
-          flexibleSpace: Stack(
-            overflow: Overflow.visible,
+  Widget build(BuildContext context) => Stack(
+        children: [
+          PageView(
             children: [
-              CustomPaint(
-                painter: BackgroundPainter(Theme.of(context)),
-                size: Size.infinite,
-              ),
-              Align(
-                alignment: Alignment(0, 0.23),
-                child: SmoothPageIndicator(
-                  controller: _controller,
-                  count: 2,
-                  effect: SwapEffect(
-                    activeDotColor: Theme.of(context).accentColor,
-                    dotColor: Colors.white38,
-                    dotHeight: 10.0,
-                    dotWidth: 10.0,
-                    radius: 5.0,
-                  ),
-                ),
-              ),
+              OrariSection(),
+              LessonsSection(),
             ],
+            controller: _controller,
+            // onPageChanged: (index) {
+            //   setState(() {
+            //     _index = index;
+            //   });
+            // },
           ),
-          bottom: PreferredSize(
-            child: Container(),
-            preferredSize:
-                Size.fromHeight(MediaQuery.of(context).size.width / 8),
-          ),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate([
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 2,
-              child: PageView(
-                children: [
-                  OrariSection(),
-                  LessonsSection(),
-                ],
-                controller: _controller,
-                // onPageChanged: (index) {
-                //   setState(() {
-                //     _index = index;
-                //   });
-                // },
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SmoothPageIndicator(
+              controller: _controller,
+              count: 2,
+              effect: SwapEffect(
+                activeDotColor: Theme.of(context).accentColor,
+                dotColor: Colors.white10,
+                dotHeight: 10.0,
+                dotWidth: 10.0,
+                radius: 5.0,
               ),
-            )
-          ]),
-        )
-      ],
-    );
+            ),
+          ),
+        ],
+      );
+}
+
+void rebuildAllChildren(BuildContext context) {
+  void rebuild(Element el) {
+    el.markNeedsBuild();
+    el.visitChildren(rebuild);
   }
+
+  (context as Element).visitChildren(rebuild);
 }
